ff5d125efd5a357b7715ea792f4d7b492f1dc092
gregoire hornung <greghor4@gmail.com>
2021-11-09 16:56:45 +0100

    add plot

--- src/playground/demo.py
+++ src/playground/demo.py
@@ -4,8 +4,20 @@ from matplotlib import pyplot as plt
 
 df = pd.read_csv("data/raw/titanic.csv")
 
-
 df["age"] = pd.to_numeric(df["age"])
+
+
+fig, ax1 = plt.subplots()
+ax1.set_ylabel("")
+ax1.set_xlabel("")
+ax1.set_title("")
+ax1.plot(df["age"])
+ax1.grid()
+plt.show(block=False)
+
+df["fare"] = pd.to_numeric(df["fare"])
+
+
 
 
 
