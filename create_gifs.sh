436da35f5b75e59812231fc493d8f1bfa0891b85
gregoire hornung <greghor4@gmail.com>
2021-11-10 15:00:53 +0100

    update

--- create_gifs.sh
+++ create_gifs.sh
@@ -1,10 +1,12 @@
 ffmpeg -ss 00:00:01 -to 00:00:20 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/register.gif -y
 
-
- ffmpeg -ss 00:02:02 -to 00:02:20 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/best_secret.gif -y
+ffmpeg -ss 00:02:02 -to 00:02:20 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/best_secret.gif -y
 
 ffmpeg -ss 00:02:26 -to 00:02:36 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/ctrlp.gif -y
 
-
 ffmpeg -ss 00:00:38 -to 00:00:48 -i data/external/records_2.mov -vf "fps=10" -loop 0 data/gif/git.gif
+
+ffmpeg -ss 00:00:38 -to 00:01:38 -i data/external/records_3.mov -vf "fps=5" -loop 0 data/gif/macro.gif -y
+
+
 
