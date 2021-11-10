bed8221d26dee63aac7ee8d56ca081c1cb4791d4
gregoire hornung <greghor4@gmail.com>
2021-11-10 15:05:43 +0100

    dummy

--- src/playground/demo.py
+++ src/playground/demo.py
@@ -6,22 +6,10 @@ df = pd.read_csv("data/raw/titanic.csv")
 
 df["age"] = pd.to_numeric(df["age"])
 
-
 fig, ax1 = plt.subplots()
 ax1.set_ylabel("")
 ax1.set_xlabel("")
 ax1.set_title("")
-ax1.plot(df["age"])
 ax1.grid()
 plt.show(block=False)
-
-df["fare"] = pd.to_numeric(df["fare"])
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 4cac8cef251266b53fb947cef559bdce1f7470d5
-
 
