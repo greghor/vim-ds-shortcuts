557710ce0937c055846af646671dd4bc6d987dfe
gregoire hornung <greghor4@gmail.com>
2021-11-11 20:55:50 +0100

    clean session

--- create_gifs.sh
+++ create_gifs.sh
@@ -1,12 +1,14 @@
-ffmpeg -ss 00:00:01 -to 00:00:20 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/register.gif -y
+ffmpeg -ss 00:00:00 -to 00:00:08 -i data/external/register_bestsecret.mov -vf "fps=10" -loop 0 data/gif/register_1.gif -y
 
-ffmpeg -ss 00:02:02 -to 00:02:20 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/best_secret.gif -y
+ffmpeg -ss 00:00:45 -to 00:00:48 -i data/external/register_bestsecret.mov -vf "fps=10" -loop 0 data/gif/register_2.gif -y
 
-ffmpeg -ss 00:02:26 -to 00:02:36 -i data/external/records_1.mov -vf "fps=10" -loop 0 data/gif/ctrlp.gif -y
+ffmpeg -ss 00:01:16 -to 00:01:29 -i data/external/best_secret.mov -vf "fps=10" -loop 0 data/gif/best_secret.gif -y
 
-ffmpeg -ss 00:00:38 -to 00:00:48 -i data/external/records_2.mov -vf "fps=10" -loop 0 data/gif/git.gif
+ffmpeg -ss 00:00:00 -to 00:00:05 -i data/external/ctrlp.mov -vf "fps=10" -loop 0 data/gif/ctrlp.gif -y
 
-ffmpeg -ss 00:00:38 -to 00:01:38 -i data/external/records_3.mov -vf "fps=5" -loop 0 data/gif/macro.gif -y
+ffmpeg -ss 00:00:03 -to 00:00:29 -i data/external/git.mov -vf "fps=10" -loop 0 data/gif/git.gif -y
+
+ffmpeg -ss 00:01:07 -to 00:01:38 -i data/external/macro.mov -vf "fps=5" -loop 0 data/gif/macro.gif -y
 
 
 
--- src/playground/demo.py
+++ src/playground/demo.py
@@ -13,15 +13,7 @@ ax1.set_title("")
 ax1.grid()
 plt.show(block=False)
 
-[
-"mario kart stadium",
-"sweet sweet canyon",
-"thwomp ruins",
-"mario circuit ",
-"toad harbor",
-"shy guy falls",
-"sunshine airport",
-"mount wario",
-"cloudtop cruise",
-"bowser's castle",
-]
+
+df["age"] = pd.to_numeric(df["age"])]
+data_path = "data/raw/titanic.csv"
+
